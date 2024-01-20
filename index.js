2f344cd77462f3085ada93f1ce54db16c1b17245
Pritam Sharma <notpritamsharma@gmail.com>
2024-01-20 22:59:58 +0530

    Working Python Code With Input Taking Also

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-const evaluateFunction = (lang, code, input) => {
+const evaluateFunction = (lang, code, input, res) => {
   let tempFile;
   let runCommand;
   let dockerCommand;
@@ -40,16 +40,39 @@ const evaluateFunction = (lang, code, input) => {
       tempFile = `temp.js`;
       fs.writeFileSync(tempFile, code, "utf-8");
 
-      runCommand = `docker run --rm -i --network none -v ${__dirname}:/code -w /code coderunner /bin/bash -c "node ${tempFile}"`;
+      runCommand = `docker run --rm -i --network none -v ${__dirname}:/code -w /code coderunner_javscript /bin/bash -c "node ${tempFile}"`;
       child_process = exec(runCommand, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing Docker command: ${error}`);
+          return res.json({ error: "Error executing Docker command." });
+        }
+        console.log("Running Docker");
         res.json({ stdout, stderr, error });
         // Remove the temporary code file
         fs.unlinkSync(tempFile);
       });
-
+      // evaluateFunction(lang, code, input, { json: (data) => console.log(data) });
       child_process.stdin.write(input);
       child_process.stdin.end();
+      break;
+    case "python":
+      tempFile = `temp.py`;
+      fs.writeFileSync(tempFile, code, "utf-8");
 
+      runCommand = `docker run --rm -i --network none -v ${__dirname}:/code -w /code coderunner_python /bin/bash -c "python ${tempFile}"`;
+
+      child_process = exec(runCommand, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing Docker command: ${error}`);
+          return res.json({ error: "Error executing Docker command." });
+        }
+        console.log("Running Docker");
+        res.json({ stdout, stderr, error });
+        // Remove the temporary code file
+        fs.unlinkSync(tempFile);
+      });
+      child_process.stdin.write(input);
+      child_process.stdin.end();
       break;
   }
 };
@@ -59,20 +82,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/evaluate", (req, res) => {
-  const code_to_run = `
-        const sum= (a,b)=>{
-            // console.log("Hello from inside the function");
-            return a+b;
-        };
-        
-        sum(3,5);
-        `;
+  console.log("Evaluating code");
+  // const lang = "javascript";
+  // const input = "5 10";
+  // const code = `console.log(4+5);`;
 
-  const lang = "javascript";
+  const lang = "python";
   const input = "5 10";
-  const code = `console.log("Hello World");`;
+  const code = `num1, num2 = map(int, input().split())\nprint(num1 + num2)`;
 
-  evaluateFunction(lang, code, input);
+  evaluateFunction(lang, code, input, { json: (data) => console.log(data) });
 });
 
 app.listen(3000, () => {
---
+++ temp.js
@@ -0,0 +1,2 @@
+num1, num2 = map(int, input().split())
+print(num1 + num2)
