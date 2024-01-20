37756e2fb7618dbcfc7c5d8e311b009559e2182d
Pritam Sharma <notpritamsharma@gmail.com>
2024-01-21 02:22:12 +0530

    Frontend Started

---
+++ frontend/app/auth/page.tsx
@@ -0,0 +1,15 @@
+const page = () => {
+  return (
+    <div>
+      <header className="w-full h-[50px] bg-white dark:bg-[#282828] flex py-[120px]">
+        <div className="flex">
+          <div className="">logo</div>
+          <div className="">Explore</div>
+        </div>
+        <div></div>
+      </header>
+    </div>
+  );
+};
+
+export default page;
